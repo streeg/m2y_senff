df3c1a1f4a9629b4594dac5617a1cc77603983ff
Guilherme Monteiro <guilherme.monteiro@mobile2you.com.br>
2022-01-24 15:24:36 -0300

    chore(SENFF):(changing version, adding list_key_claim ep)

--- lib/m2y_senff/constants/constants.rb
+++ lib/m2y_senff/constants/constants.rb
@@ -1,66 +1,65 @@
 module M2ySenff
-
-  #banks
+  # banks
   BANK_ID = 1414
   INSTITUTION_ID = 1414
 
-  #pix
-  SCOPE = "qrcode_api"
-  GRANT_TYPE = "client_credentials"
-  PIX_AUTH_PATH = "/auth/oauth2/token"
-  PIX_ADD_KEY_PATH = "/dict/chave/incluir/"
-  PIX_REMOVE_KEY_PATH = "/dict/chave/excluir/"
-  PIX_LIST_KEYS_PATH = "/dict/chave/listar/chave"
+  # pix
+  SCOPE = 'qrcode_api'
+  GRANT_TYPE = 'client_credentials'
+  PIX_AUTH_PATH = '/auth/oauth2/token'
+  PIX_ADD_KEY_PATH = '/dict/chave/incluir/'
+  PIX_REMOVE_KEY_PATH = '/dict/chave/excluir/'
+  PIX_LIST_KEYS_PATH = '/dict/chave/listar/chave'
+  PIX_LIST_CLAIM_KEY_PATH = '/dict/chave/reivindicacao/listar'
   ### Paths
 
-  #account
-  DEPOSIT_PATH = "v1/accounts/deposits"
-  PAYMENT_PATH = "v1/payments"
+  # account
+  DEPOSIT_PATH = 'v1/accounts/deposits'
+  PAYMENT_PATH = 'v1/payments'
 
-  STATEMENT = "/statement"
-  WITHDRAW = "/withdraw"
+  STATEMENT = '/statement'
+  WITHDRAW = '/withdraw'
 
-  BANKS_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0102A1/listarBancos"
-  ACCOUNT_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0102M/obterContas"
-  USER_PATH = "/API/BJ08M01/user"
-  INDIVIDUAL_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101L/buscaDadosCliente"
-  CUSTOMERS_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0107A/obterClientes"
-  EXTRACT_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101C/listaLancamentos"
-  FUTURE_EXTRACT_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101F/listaLancFuturos"
-  RECEIPTS_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101M/obterComprovantes"
-  FIND_RECEIPTS_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101N/criarComprovante"
-  TRANSFER_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0101K/efetuaLancamentoTransferencia"
-  ADD_FAV_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0102O/manutencaoCadastrarFavorecido"
-  CHECK_FAV_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0102O/manutencaoListarFavorecidos"
-  RECEIPTS_DETAILS = "/API/BJ08M01/BJ08M01/BJ08SS0101O/consultaComprovanteDetalhados"
+  BANKS_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0102A1/listarBancos'
+  ACCOUNT_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0102M/obterContas'
+  USER_PATH = '/API/BJ08M01/user'
+  INDIVIDUAL_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101L/buscaDadosCliente'
+  CUSTOMERS_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0107A/obterClientes'
+  EXTRACT_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101C/listaLancamentos'
+  FUTURE_EXTRACT_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101F/listaLancFuturos'
+  RECEIPTS_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101M/obterComprovantes'
+  FIND_RECEIPTS_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101N/criarComprovante'
+  TRANSFER_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0101K/efetuaLancamentoTransferencia'
+  ADD_FAV_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0102O/manutencaoCadastrarFavorecido'
+  CHECK_FAV_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0102O/manutencaoListarFavorecidos'
+  RECEIPTS_DETAILS = '/API/BJ08M01/BJ08M01/BJ08SS0101O/consultaComprovanteDetalhados'
 
-  #BILLETTS
-  BILLETS_PATH = "/API/BJ08M01/BJ08M01/BJ08SS0102G/gerarBoleto"
+  # BILLETTS
+  BILLETS_PATH = '/API/BJ08M01/BJ08M01/BJ08SS0102G/gerarBoleto'
 
-  #PAYMENTS
-  VALIDATE_EAN = "/API/BJ08M01/BJ08M01/BJ08SS0102F/validarBoleto"
-  PAY_EAN = "/API/BJ08M01/BJ08M01/BJ08SS0102E/pagarBoleto"
+  # PAYMENTS
+  VALIDATE_EAN = '/API/BJ08M01/BJ08M01/BJ08SS0102F/validarBoleto'
+  PAY_EAN = '/API/BJ08M01/BJ08M01/BJ08SS0102E/pagarBoleto'
 
+  # DEG
+  ADD_KIT = '/BJ79M02/BJ79M02/BJ79SS0101B/kitdoc'
+  CHECK_KIT = '/BJ79M02/BJ79M02/BJ79SS0101B/consultaKit'
+  SEND_PROPOSAL = '/BJ79M02/BJ79M02/BJ79SS0101A/proposta'
 
-  #DEG
-  ADD_KIT = "/BJ79M02/BJ79M02/BJ79SS0101B/kitdoc"
-  CHECK_KIT = "/BJ79M02/BJ79M02/BJ79SS0101B/consultaKit"
-  SEND_PROPOSAL = "/BJ79M02/BJ79M02/BJ79SS0101A/proposta"
+  CHECK_ADDRESS = '/BJ01M06/clientesEndereco/pesquisa'
+  CHECK_CUSTOMERS = '/BJ01M06/clientes/pesquisa'
+  USER_ADDRESS_PATH = '/BJ01M06/user'
 
-  CHECK_ADDRESS = "/BJ01M06/clientesEndereco/pesquisa"
-  CHECK_CUSTOMERS = "/BJ01M06/clientes/pesquisa"
-  USER_ADDRESS_PATH = "/BJ01M06/user"
+  # recharge
+  RECHARGE_AUTH_PATH = '/connect/token'
+  DEALERS_PATH = '/api/mobile-rechange/operator'
+  RECHARGES_RECEIPTS = '/api/cellphone-topups'
+  RECHARGES_PACKAGES = '/api/mobile-rechange/refill-request'
+  RECHARGE = '/api/mobile-rechange/request-recharge/pj'
 
-  #recharge
-  RECHARGE_AUTH_PATH = "/connect/token"
-  DEALERS_PATH = "/api/mobile-rechange/operator"
-  RECHARGES_RECEIPTS = "/api/cellphone-topups"
-  RECHARGES_PACKAGES = "/api/mobile-rechange/refill-request"
-  RECHARGE = "/api/mobile-rechange/request-recharge/pj"
-
-  #payments
-  PAYMENT_AUTH_PATH = "/connect/token"
-  VALIDATE_PATH = "/api/bill-payments/"
-  PAYMENTS_RECEIPTS = "/api/payments"
-  PAY = "/api/bill-payments/pj"
+  # payments
+  PAYMENT_AUTH_PATH = '/connect/token'
+  VALIDATE_PATH = '/api/bill-payments/'
+  PAYMENTS_RECEIPTS = '/api/payments'
+  PAY = '/api/bill-payments/pj'
 end
--- lib/m2y_senff/modules/senff_pix_keys.rb
+++ lib/m2y_senff/modules/senff_pix_keys.rb
@@ -1,7 +1,5 @@
 module M2ySenff
-
   class SenffPixKeys < SenffModule
-
     def initialize(client_id, client_secret, url, scope, auth_url)
       @auth = SenffAuth.new(client_id, client_secret, url)
       @client_id = client_id
@@ -11,21 +9,31 @@ module M2ySenff
     end
 
     def refreshToken
-      if SenffHelper.shouldRefreshToken?(@client_secret)
-        @auth.generateToken
-      end
+      @auth.generateToken if SenffHelper.shouldRefreshToken?(@client_secret)
     end
-
 
     def list_keys(body)
       refreshToken
       url = @url + PIX_LIST_KEYS_PATH
       headers = json_headers
-      headers["Authorization"] = "Bearer #{SenffHelper.get_token(@client_secret)}"
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{SenffHelper.get_token(@client_secret)}"
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         SenffModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
+        nil
+      end
+    end
+
+    def list_claim_keys(body)
+      refreshToken
+      url = @url + PIX_LIST_CLAIM_KEY_PATH
+      headers = json_headers
+      headers['Authorization'] = "Bearer #{SenffHelper.get_token(@client_secret)}"
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
+      begin
+        SenffModel.new(req.parsed_response)
+      rescue StandardError
         nil
       end
     end
@@ -34,31 +42,29 @@ module M2ySenff
       refreshToken
       url = @url + PIX_ADD_KEY_PATH
       headers = json_headers
-      headers["Authorization"] = "Bearer #{SenffHelper.get_token(@client_secret)}"
-      headers["Chave-Idempotencia"] = SecureRandom.uuid
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{SenffHelper.get_token(@client_secret)}"
+      headers['Chave-Idempotencia'] = SecureRandom.uuid
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         SenffModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
         nil
       end
     end
 
     def remove_key(id, body)
       refreshToken
-      url = @url + PIX_REMOVE_KEY_PATH + "#{id}"
+      url = @url + PIX_REMOVE_KEY_PATH + id.to_s
       puts url
       headers = json_headers
-      headers["Authorization"] = "Bearer #{SenffHelper.get_token(@client_secret)}"
-      headers["Chave-Idempotencia"] = SecureRandom.uuid
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{SenffHelper.get_token(@client_secret)}"
+      headers['Chave-Idempotencia'] = SecureRandom.uuid
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         SenffModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
         nil
       end
     end
-
   end
-
 end
