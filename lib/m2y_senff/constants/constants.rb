20801696f52bef196719adaf1dd9058180be7228
guilhermeandreuce <guilherme.monteiro@mobile2you.com.br>
2022-03-16 18:01:28 -0300

    chore(SENFF):(adding missing files from export)

--- lib/m2y_senff/constants/constants.rb
+++ lib/m2y_senff/constants/constants.rb
@@ -17,6 +17,7 @@ module M2ySenff
   PIX_COMPLETE_KEY_CLAIM_PATH = '/dict/chave/reivindicacao/'
   PIX_VALIDATE_KEY_PATH = '/dict/chave/consultar/'
   PIX_TRANSFER_PATH = '/spi/api/v2/op'
+  PIX_STATIC_QRCODE_PATH = '/qrcode/estatico/gerar'
   ### Paths
 
   # account
